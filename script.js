38cbe27f420524b72946754f74add00922179558
Yakshith <yakshith.kommineni@gmail.com>
2024-07-17 18:07:37 -0400

    more dynamics

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-const phrases = [
-    "I'm A Developer.",
-    "I'm A Student.",
-    "I'm An Innovator."
-];
-
-let currentIndex = 0;
-const heading = document.getElementById('animated-heading');
-
-function changePhrase() {
-    currentIndex = (currentIndex + 1) % phrases.length;
-    heading.textContent = phrases[currentIndex];
-}
-
-setInterval(changePhrase, 2000)
 
 var tablinks = document.getElementsByClassName("tab-links");
 var tabcontents = document.getElementsByClassName("tab-contents");
