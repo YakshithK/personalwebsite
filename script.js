029b2b0472714d460c81999cd1df18ddcc8f3110
Yakshith <yakshith.kommineni@gmail.com>
2024-07-26 19:35:13 -0400

    added categorization for my work

--- script.js
+++ script.js
@@ -1,18 +1,3 @@
-
-var tablinks = document.getElementsByClassName("tab-links");
-var tabcontents = document.getElementsByClassName("tab-contents");
-
-function opentab(tabname){
-    for(tablink of tablinks){
-        tablink.classList.remove("active-link");
-    }
-    for(tabcontent of tabcontents){
-        tabcontent.classList.remove("active-tab");
-    }
-    event.currentTarget.classList.add("active-link");
-    document.getElementById(tabname).classList.add("active-tab")
-}
-
 var sidemenu = document.getElementById("sidemenu");
 
 function openmenu(){
@@ -21,4 +6,14 @@ function openmenu(){
 
 function closemenu(){
     sidemenu.style.right = "-200px";
+}
+
+var work_all = document.getElementsByClassName("work-row")
+
+function opentab(tabname){
+    for (work of work_all){
+        work.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-tab")
+    document.getElementById(tabname).classList.add("active-tab")
 }
