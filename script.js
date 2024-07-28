8b1491f0a28ac9a875661deff0663985eb1c6c3f
Yakshith <yakshith.kommineni@gmail.com>
2024-07-28 14:14:08 -0400

    added pagnation

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var sidemenu = document.getElementById("sidemenu");
 var cardsper_page = 4;
+var num = 0
 
 function openmenu() {
     sidemenu.style.right = "0";
@@ -11,17 +12,31 @@ function closemenu() {
 
 var work_all = document.getElementsByClassName("work-row");
 
-function opentab(tabname) {
+function opentab(tabname, int) {
+
     for (var work of work_all) {
         work.classList.remove("active-tab");
     }
     document.getElementById(tabname).classList.add("active-tab");
 
-    // Get the active tab element
-    var activeTab = document.getElementById(tabname);
-
-    var workCards = activeTab.getElementsByClassName("work");
-    var totalWorkCards = workCards.length;
-    var totalPages = Math.ceil(totalWorkCards / cardsper_page);
 }
 
+function displayPage(int){
+    num = num + int
+    end = num * cardsper_page
+    start = end - cardsper_page
+    var active_tab = document.getElementsByClassName('active-tab');
+    
+    var workCards = active_tab[0].getElementsByClassName("work");
+    var totalWorkCards = workCards.length;
+
+    if (workCards.length > cardsper_page){
+        for (card of workCards){
+            card.style.display = 'none'
+        }
+        workCards[start].style.display = ''
+        workCards[start + 1].style.display = ''
+        workCards[start + 2].style.display = ''
+        workCards[start + 3].style.display = ''
+    }
+}
