c245fd53c92fcd6231a69dad3e8764746f49391e
Yakshith <yakshith.kommineni@gmail.com>
2024-07-28 01:20:52 -0400

    UI

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
 var sidemenu = document.getElementById("sidemenu");
+var cardsper_page = 4;
 
-function openmenu(){
+function openmenu() {
     sidemenu.style.right = "0";
 }
 
-function closemenu(){
+function closemenu() {
     sidemenu.style.right = "-200px";
 }
 
-var work_all = document.getElementsByClassName("work-row")
+var work_all = document.getElementsByClassName("work-row");
 
-function opentab(tabname){
-    for (work of work_all){
+function opentab(tabname) {
+    for (var work of work_all) {
         work.classList.remove("active-tab");
     }
-    event.currentTarget.classList.add("active-tab")
-    document.getElementById(tabname).classList.add("active-tab")
+    document.getElementById(tabname).classList.add("active-tab");
+
+    // Get the active tab element
+    var activeTab = document.getElementById(tabname);
+
+    var workCards = activeTab.getElementsByClassName("work");
+    var totalWorkCards = workCards.length;
+    var totalPages = Math.ceil(totalWorkCards / cardsper_page);
 }
+
